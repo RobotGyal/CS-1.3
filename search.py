6a68f5d1dbacf8efa8c67e1bbb2bca4d1f0ab0c5
Aleia <aleiaknight@gmail.com>
2020-04-28 20:19:06 -0700

    all search tests passed

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ def linear_search(array, item):
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
     return linear_search_iterative(array, item)
-    # return linear_search_recursive(array, item)
+    return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
@@ -16,10 +16,17 @@ def linear_search_iterative(array, item):
 
 
 def linear_search_recursive(array, item, index=0):
-    # TODO: implement linear search recursively here
-    pass
-    # once implemented, change linear_search to call linear_search_recursive
-    # to verify that your recursive implementation passes all tests
+    # if we hit the end, return nothing
+    # if location arr[loc] is the item, return the location
+    # to move through, call function
+    end = len(array)-1
+    if index > end:
+        return None
+    if array[index] == item:
+        return index
+    if array[end] == item:
+        return end
+    return linear_search_recursive(array, item, index+1)
 
 
 def binary_search(array, item):
@@ -27,7 +34,7 @@ def binary_search(array, item):
     # implement binary_search_iterative and binary_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
     return binary_search_iterative(array, item)
-    # return binary_search_recursive(array, item)
+    return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
@@ -39,7 +46,7 @@ def binary_search_iterative(array, item):
     '''
     loc=0
     end=len(array)-1
-    while loc <= r: 
+    while loc <= end: 
         mid = loc + (end - loc)//2; 
         if array[mid] == item: 
             return mid
@@ -51,7 +58,16 @@ def binary_search_iterative(array, item):
 
 
 def binary_search_recursive(array, item, left=None, right=None):
-    # TODO: implement binary search recursively here
-    pass
-    # once implemented, change binary_search to call binary_search_recursive
-    # to verify that your recursive implementation passes all tests
+    if left is None and right is None:
+        left=0
+        right =len(array)-1
+
+    if right >= left:
+        mid = left + ((right - left)//2)
+        if array[mid] == item:
+            return mid
+        elif array[mid] > item:
+            return binary_search_recursive(array, item, left, mid-1)
+        elif array[mid] < item:
+            return binary_search_recursive(array, item, mid+1, right)
+    return None
